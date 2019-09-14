378fc63b91eb3f5c6019261abeac4fec6e041102
Craig S. Cottingham <craig@cottingham.net>
2019-09-13 19:57:44 -0500

    Restructure upgrader to be more like other roles

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,15 +1,24 @@
 var roleUpgrader = {
   run: function(creep) {
-    if (creep.room.controller) {
-      switch (creep.upgradeController(creep.room.controller)) {
-        case ERR_NOT_ENOUGH_RESOURCES:
-          creep.memory.role = 'harvester';
-          break;
-        case ERR_NOT_IN_RANGE:
+    if (creep.carry.energy == 0) {
+      creep.memory.role = 'harvester';
+      return OK;
+    }
+
+    var target = creep.room.controller;
+    if (target === undefined) {
+      creep.memory.role = 'builder';
+      return OK;
+    }
+
+    switch (creep.upgradeController(creep.room.controller)) {
+      case ERR_NOT_ENOUGH_RESOURCES:
+        creep.memory.role = 'harvester';
+        break;
+      case ERR_NOT_IN_RANGE:
         creep.say("upgrade");
-          creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#0000ff'}});
-          break;
-      }
+        creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#0000ff'}});
+        break;
     }
 
     return OK;
