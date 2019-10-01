ecb7efdbfd2017b0a9c794719e58a5af2fe8345b
Craig S. Cottingham <craig@cottingham.net>
2019-09-30 23:35:15 -0500

    Rework repairing code for creeps and towers

--- src/tower.js
+++ src/tower.js
@@ -3,6 +3,7 @@ var tower = {
     var target;
     var pos = tower.pos;
 
+    // attack hostile creeps with HEAL
     target = pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
       filter: (c) => _.any(c.body, "type", HEAL)
     });
@@ -11,12 +12,14 @@ var tower = {
       return OK;
     }
 
+    // attack hostile creeps without HEAL
     target = pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     if (target !== null) {
       tower.attack(target);
       return OK;
     }
 
+    // heal our own creeps
     target = pos.findClosestByRange(FIND_CREEPS, {
       filter: (c) => (c.hits < c.hitsMax)
     });
@@ -25,6 +28,8 @@ var tower = {
       return OK;
     }
 
+    // repair new structures up to a minimum safe level (so they don't decay away)
+    // (and yes I'm aware that walls don't decay)
     target = pos.findClosestByRange(FIND_STRUCTURES, {
       filter: (s) => ((s.structureType == STRUCTURE_RAMPART) || (s.structureType == STRUCTURE_WALL)) && (s.hits <= (RAMPART_DECAY_AMOUNT * 5))
     });
@@ -33,20 +38,29 @@ var tower = {
       return OK;
     }
 
+    // repair ramparts that are below the low water threshold
     target = pos.findClosestByRange(FIND_STRUCTURES, {
-      filter: (s) => {
-        switch (s.structureType) {
-          case STRUCTURE_RAMPART:
-            return (s.hits < (Memory.defenseLowWater[tower.room.name][STRUCTURE_RAMPART] - (TOWER_POWER_REPAIR * TOWER_FALLOFF)));
-          case STRUCTURE_WALL:
-            return (
-              (tower.room.find(FIND_MY_CREEPS, {filter: (c) => (c.memory.assignment == s.id)}).length == 0) &&
-              (s.hits < (Memory.defenseLowWater[tower.room.name][STRUCTURE_WALL] - (TOWER_POWER_REPAIR * TOWER_FALLOFF)))
-            );
-          default:
-            return (s.hits < (s.hitsMax - (TOWER_POWER_REPAIR * TOWER_FALLOFF)));
-        }
-      }
+      filter: (s) => (s.structureType == STRUCTURE_RAMPART) && (s.hits < (Memory.defenseLowWater[tower.room.name][STRUCTURE_RAMPART] - (TOWER_POWER_REPAIR * TOWER_FALLOFF)))
+    });
+    if (target !== null) {
+      tower.repair(target);
+      return OK;
+    }
+
+    // repair other structures (besides ramparts and walls)
+    target = pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: (s) => (s.structureType != STRUCTURE_RAMPART) && (s.structureType != STRUCTURE_WALL) && (s.hits < (s.hitsMax - (TOWER_POWER_REPAIR * TOWER_FALLOFF)))
+    });
+    if (target !== null) {
+      tower.repair(target);
+      return OK;
+    }
+
+    // repair walls that are below the low water threshold
+    target = pos.findClosestByRange(FIND_STRUCTURES, {
+      // TODO: filter out walls being breached?
+      //       (tower.room.find(FIND_MY_CREEPS, {filter: (c) => (c.memory.assignment == s.id)}).length == 0)
+      filter: (s) => (s.structureType == STRUCTURE_WALL) && (s.hits < (Memory.defenseLowWater[tower.room.name][STRUCTURE_WALL] - (TOWER_POWER_REPAIR * TOWER_FALLOFF)))
     });
     if (target !== null) {
       tower.repair(target);
