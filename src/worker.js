df0f8dc1986bfc1de01cac78d63a4fb73f33cfd2
Craig S. Cottingham <craig@cottingham.net>
2019-10-18 23:49:13 -0500

    Remove stat functions from worker module

--- src/worker.js
+++ src/worker.js
@@ -43,40 +43,6 @@ let worker = {
       // spawn.room.visual.text("🛠️ Worker", spawn.pos.x + 1, spawn.pos.y, {align: "left", opacity: 0.8});
     }
     return result;
-  },
-
-  //
-  // stats reporting functions
-  //
-
-  activeCount: function() {
-    return (this.totalCount() - this.spawningCount());
-  },
-
-  deadCount: function() {
-    return _.reduce(Memory.creeps, function(count, creep, name) {
-      if (!Game.creeps[name]) {
-        return count + 1;
-      }
-      else {
-        return count;
-      }
-    }, 0);
-  },
-
-  spawningCount: function() {
-    return _.reduce(Game.spawns, function(count, spawn) {
-      if (spawn.spawning) {
-        return count + 1;
-      }
-      else {
-        return count;
-      }
-    }, 0);
-  },
-
-  totalCount: function() {
-    return _.values(Game.creeps).length;
   }
 }
 
