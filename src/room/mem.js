b503a30fb94445da767f6cfe3475e44a0c4ac7c5
Craig S. Cottingham <craig@cottingham.net>
2019-10-27 00:29:15 -0500

    Apparently Memory.rooms doesn’t exist yet
    So we have to define it ourselves.

--- src/room/mem.js
+++ src/room/mem.js
@@ -2,10 +2,12 @@
 
 Object.defineProperty(Room.prototype, "mem", {
   get: function() {
-    return Memory.rooms[this.name] = Memory.rooms[this.name] || {};
+    // return Memory.rooms[this.name] = Memory.rooms[this.name] || {};
+    return this.memory = this.memory || {};
   },
   set: function(value) {
-    Memory.rooms[this.name] = value;
+    // Memory.rooms[this.name] = value;
+    this.memory = value;
   },
   configurable: true,
 });
